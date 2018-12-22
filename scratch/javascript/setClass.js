64f4999ea8f85821a802db968600810df002a2dc
kazam zahedi <kazam.zahedi@powercoders.org>
2018-12-22 23:24:45 +0100

    change the javascript CODE
    make a easier code and shorter

--- scratch/javascript/setClass.js
+++ scratch/javascript/setClass.js
@@ -1,16 +1,37 @@
 //Javascript code goes here
-console.log('My code is running');
+//console.log('My code is running');
 
-function greet(name) {
-  console.log('hello, ' + name);
+//function greet(name) {
+//  console.log('hello, ' + name);
+//}
+/////////////////////////////////////
+//console.log('This is the first JS line');
+function setClass(className) {
+//    console.log('setClass is running');
+
+//  let el = document.getElementById('mypara')
+//  el.className = className;
+    let el = document.getElementById('mypara').className = className;
 }
 
-function setClass(event, className) {
+//console.log('This is after setClass is defined');
 
-  let el = document.getElementById('mypara');
-  el.className = className;
-
-}
+document.addEventListener('DOMContentLoaded', function () {
+//    console.log('DOMContentLoaded has fired');
+//moder aval-1
+    document.getElementById('make-warning').addEventListener('click', function(event) {
+//        console.log('click listener fired');
+        setClass( 'warning');
+    });
+    document.getElementById('make-tip').addEventListener('click', function (event) {
+        setClass('tip');
+    });
+//model dovom-2 asantar baray khandan code ha
+    let makeClear = document.getElementById('make-clear');
+    makeClear.addEventListener('click', function(event) {
+        setClass('');
+    });
+});
 
 //function setClassTip(event) {
 // let el = document.getElementById('mypara');
