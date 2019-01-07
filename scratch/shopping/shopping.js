7595f11eba8c59f65e241360c5e495dbac3e5f40
kazam zahedi <kazam.zahedi@powercoders.org>
2019-01-07 16:17:08 +0100

    create (list) and (button)

--- scratch/shopping/shopping.js
+++ scratch/shopping/shopping.js
@@ -3,10 +3,21 @@ document.addEventListener('DOMContentLoaded', function(event) {
         console.log('The button was clicked');
         let inputBox = document.getElementById('item');
         inputBox.value;
-        let addlist = document.createElement("li");
-        let textli = document.createTextNode(inputBox.value);
-        addlist.appendChild(textli);
-        document.getElementById("list").appendChild(addlist);
+        function creatNesListItem() {
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+            let button = document.createElement('button');
+
+            let text_span = document.createTextNode(inputBox.value);
+            let text_button = document.createTextNode('delete');
+
+            span.appendChild(text_span);
+            button.appendChild(text_button);
+            li.appendChild(span);
+            li.appendChild(button);
+            document.querySelector('ul').appendChild(li);
+        };
+        creatNesListItem();
 
     });
 });
