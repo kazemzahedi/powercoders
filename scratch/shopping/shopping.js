97226e78067ec54939c94f2f01c404edb0970d19
kazam zahedi <kazam.zahedi@powercoders.org>
2019-01-10 14:39:02 +0100

    allow the user to add an item by pressing the Enter key

--- scratch/shopping/shopping.js
+++ scratch/shopping/shopping.js
@@ -25,8 +25,17 @@ function creatNewListItem(itemName) {
 document.addEventListener('DOMContentLoaded', function (event) {
   document.querySelector('button').addEventListener('click', function (event) {
     let inputBox = document.getElementById('item');
-    let inputValue = inputBox.value;
-    let li = creatNewListItem(inputValue);
+    let li = creatNewListItem(inputBox.value);
     document.querySelector('ul').appendChild(li);
+  });
+  //Add code here that listen for 'keyup' on the 'input' element
+  document.querySelector('input').addEventListener('keyup', function (event) {
+    //and logs the event.key property
+    if (event.key === 'Enter') {
+      let inputBox = document.getElementById('item');
+      let li = creatNewListItem(inputBox.value);
+      document.querySelector('ul').appendChild(li);
+      //console.log(event.key);
+    }
   });
 });
