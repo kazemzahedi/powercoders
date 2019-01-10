500b77d616bc713123a2f9bbe8e6574f14964e48
kazam zahedi <kazam.zahedi@powercoders.org>
2019-01-10 11:05:54 +0100

    clear and easy to read with wright a new code
    --Add delete button

--- scratch/shopping/shopping.js
+++ scratch/shopping/shopping.js
@@ -1,24 +1,32 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('button').addEventListener('click', function (event) {
-        console.log('The button was clicked');
-        let inputBox = document.getElementById('item');
-        inputBox.value;
-        function creatNesListItem() {
-            let li = document.createElement('li');
-            let span = document.createElement('span');
-            let button = document.createElement('button');
+function creatNewListItem(itemName) {
+  let li = document.createElement('li');
 
-            let text_span = document.createTextNode(inputBox.value);
-            let text_button = document.createTextNode('delete');
+  let span = document.createElement('span');
+  let text_span = document.createTextNode(itemName);
 
-            span.appendChild(text_span);
-            button.appendChild(text_button);
-            li.appendChild(span);
-            li.appendChild(button);
-            document.querySelector('ul').appendChild(li);
-        };
-        creatNesListItem();
 
-    });
+  let deleteButton = document.createElement('button');
+  let delete_text = document.createTextNode('delete'); //delete_text.textContent = 'delete';
+
+  //Add a click handler that logs the click here
+  deleteButton.addEventListener('click', function (event) {
+    console.log('Delete button clicked: ' + itemName);
+    li.remove();
+  }); // this function can call the all of variable,that is in up.
+
+  span.appendChild(text_span);
+  deleteButton.appendChild(delete_text);
+
+  li.appendChild(span);
+  li.appendChild(deleteButton);
+  return li;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  document.querySelector('button').addEventListener('click', function (event) {
+    let inputBox = document.getElementById('item');
+    let inputValue = inputBox.value;
+    let li = creatNewListItem(inputValue);
+    document.querySelector('ul').appendChild(li);
+  });
 });
-
