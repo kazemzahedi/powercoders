a389e07ac5ea04bdce9dcd464f095e6becc280f4
kazam zahedi <kazam.zahedi@powercoders.org>
2019-02-05 15:54:02 +0100

    --who can add item in List with click button

--- shopping-mvc/src/js/view.js
+++ shopping-mvc/src/js/view.js
@@ -16,6 +16,16 @@ class View {
     /** @private {!HTMLElement} Shopping list element */
     this.shoppingList_ = document.querySelector('ul');
 
+    /** @private {!HTMLElement} Input widget for items */
+    this.inputBox_ = document.getElementById('item');
+
+    /** @private {!HTMLElement} Input widget for quantity */
+    this.quantityBox_ = document.getElementById('quantity');
+
+    /** @private {!HTMLElement} Button to add an item */
+    this.addItemButton_ = document.getElementById('add');
+
+    this.addItemButton_.addEventListener('click', () => this.addItem());
   }
   /**
    * Update the UI with the shopping list contents.
@@ -30,5 +40,19 @@ class View {
       const listItem = item.toListItem();
       this.shoppingList_.appendChild(listItem);
     }
+
+    this.inputBox_.value = '';
+    this.quantityBox_.value = '';
+    this.inputBox_.focus();
+  }
+
+  /**
+   * Notifies the Controller to add an item to the list.
+   */
+  addItem() {
+    const trimmedValue = this.inputBox_.value.trim();
+    const trimmedQuantity = this.quantityBox_.value.trim();
+
+    this.controller_.addItem(trimmedValue, trimmedQuantity);
   }
 }
