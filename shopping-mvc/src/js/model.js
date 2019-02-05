a06f87f5aff9eb9eb96cf3aa7f7158f824407e3e
kazam zahedi <kazam.zahedi@powercoders.org>
2019-02-05 16:53:15 +0100

    --Delete the (i) item from the list

--- shopping-mvc/src/js/model.js
+++ shopping-mvc/src/js/model.js
@@ -31,4 +31,14 @@ class Model {
     this.view_.update();
   }
 
+  /**
+   * Delete the i'th item from the list.
+   *
+   * @param i {number}
+   */
+  delete(i) {
+    this.items_.splice(i, 1);
+    this.view_.update();
+  }
+
 }
