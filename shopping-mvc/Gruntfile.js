e6e90dcce8df10b39f0e4b9425113d238e675016
kazam zahedi <kazam.zahedi@powercoders.org>
2019-02-08 16:33:34 +0100

    copy the static directori and html

--- shopping-mvc/Gruntfile.js
+++ shopping-mvc/Gruntfile.js
@@ -10,13 +10,28 @@ module.exports = function (grunt) {
         src: ['src/js/*.js', '!src/js/ui.js', 'src/js/ui.js'],
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    copy: {
+      html: {
+        src: 'src/html/index.prod.html',
+        dest: 'build/index.html'
+      },
+      static: {
+        files: [{
+            expand: true,
+            cwd: 'src/static',
+            src: '*.*',
+            dest: 'build/static'
+        }]
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'copy']);
 
 };
