bb4c9bac17b952b8059a3a440f51db54b23db495
kazam zahedi <kazam.zahedi@powercoders.org>
2019-02-14 15:39:13 +0100

    Add css file Add less file Add watch

--- shopping-mvc/Gruntfile.js
+++ shopping-mvc/Gruntfile.js
@@ -24,14 +24,53 @@ module.exports = function (grunt) {
             dest: 'build/static'
         }]
       }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'build/css/style.min.css': ['src/css/*.css']
+        }
+      }
+    },
+    less: {
+      build: {
+        files: {
+          'src/css/style.css': 'src/css/style.less'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'src/css/*.css',
+        tasks: ['cssmin']
+      },
+      scripts: {
+        files: 'src/js/*.js',
+        tasks: ['uglify']
+      },
+      html: {
+        files: 'src/html/index.prod.html',
+        tasks: ['copy:html']
+      },
+      static: {
+        files: 'src/static/*.*',
+        tasks: ['copy:static']
+      },
+      less: {
+        files: 'src/css/*.less',
+        tasks: ['less']
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'copy']);
+  grunt.registerTask('default', ['uglify', 'copy', 'less', 'cssmin']);
 
 };
